3969cdfd5228313d90448ca17465cd8da8b19ca9
Jeff Daffern <jeffdaffern@yahoo.com>
2022-01-10 00:09:23 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function happyBirthday()
+{
+	var name = prompt("What is your name?");
+	var age = prompt("What is your age?");
+
+	if (age < 50)
+	alert("Happy Birthday " + name + "! You are " + age + " years old. You're still young!");
+	else
+	alert("Happy Birthday " + name + "! You are " + age + " years old. Dang! You're Old!");
+}
+function happyme(){alert("Happy Birthday " + name + "! You are " + age + " years old. Dang! You're Old!");}
